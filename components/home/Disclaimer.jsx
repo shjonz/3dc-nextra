6adf7764c35d3318784aa9a364c9267bd1a49462
ryanng180 <ryanng180@gmail.com>
2023-05-07 05:18:55 +0800

    fourth commit

--- components/home/Disclaimer.jsx
+++ components/home/Disclaimer.jsx
@@ -2,16 +2,30 @@ export default function Disclaimer() {
   return (
     <div
       style={{
-        textAlign: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
         fontSize: "0.7rem",
         padding: "0rem",
         maxWidth: "100%",
         wordWrap: "break-word",
       }}
     >
-      Disclaimer: DSC is an independent group; the activities and opinions
-      expressed by the group should in no way be linked to Google, the
-      corporation.
+      <img
+        src="imgs\logos\GDSC.webp"
+        alt="Image description"
+        style={{
+          marginRight: "0.5rem",
+          maxHeight: "2em",
+          maxWidth: "100%",
+          height: "auto",
+        }}
+      />
+      <span>
+        Disclaimer: DSC is an independent group; the activities and opinions
+        expressed by the group should in no way be linked to Google, the
+        corporation.
+      </span>
     </div>
   );
 }
